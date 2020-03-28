191a79026d22f11f785e8a64739196fceb0e01f9
Pete Martin <petemartinuk@gmail.com>
2020-03-28 14:59:12 -0100

    internationalisation

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,6 +3,13 @@ import { inject as service } from '@ember/service';
 
 export default class ApplicationRoute extends Route {
   @service store
+  @service intl
+
+  beforeModel() {
+    this._super(...arguments);
+    return this.intl.setLocale('pt_pt');
+  }
+
   async model() {
     return this.store.findAll('update').then(function(results) {
 
