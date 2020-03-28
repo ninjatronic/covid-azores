fea79eb29ce0204712b83642ee7376bc5ea6e88f
Pete Martin <petemartinuk@gmail.com>
2020-03-28 22:51:17 -0100

    integration test for snapshot component

--- tests/integration/components/snapshot-test.js
+++ tests/integration/components/snapshot-test.js
@@ -2,25 +2,50 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { A } from '@ember/array';
 
 module('Integration | Component | snapshot', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  let mockModel = [{
+      id: 1,
+      date: new Date(),
+      saoMiguel: 0,
+      santaMaria: 1,
+      faial: 2,
+      terceira: 3,
+      pico: 4,
+      saoJorge: 5,
+      graciosa: 6,
+      flores: 7,
+      corvo: 8,
+      total: 9
+    },{
+      id: 2,
+      date: new Date(),
+      saoMiguel: 0,
+      santaMaria: 1,
+      faial: 2,
+      terceira: 3,
+      pico: 4,
+      saoJorge: 5,
+      graciosa: 6,
+      flores: 7,
+      corvo: 8,
+      total: 9
+    }];
+
+  test('it renders correctly', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Snapshot />`);
+    this.set('model', mockModel);
 
-    assert.equal(this.element.textContent.trim(), '');
+    await render(hbs`<Snapshot @data={{this.model}}/>`);
 
-    // Template block usage:
-    await render(hbs`
-      <Snapshot>
-        template block text
-      </Snapshot>
-    `);
+    assert.equal(this.element.textContent.trim().replace(/\s\s+/g, " "),
+    'Total São Miguel Santa Maria Faial Terceira Pico São Jorge Graciosa Flores Corvo 18 0 2 4 6 8 10 12 14 16');
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    console.log(this.element.querySelector('td'))
   });
 });
