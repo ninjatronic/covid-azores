088f9c11c5f60a319193f8c0e8a07a07d612f462
Pete Martin <petemartinuk@gmail.com>
2020-03-28 14:14:54 -0100

    fixed application adapter to include rootURL

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,10 +1,10 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
-import config from './config/environment';
+import config from 'covid-azores/config/environment';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
   namespace = 'api';
 
   buildURL(...args) {
-    return `${config.rootURL}/${super.buildURL(...args)}.json`;
+    return `${config.rootURL}${super.buildURL(...args).replace(/^\/+/, '')}.json`;
   }
 }
