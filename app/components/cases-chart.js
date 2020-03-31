29d0983f4ba80ef716f327bf94b43abff799dc7b
Pete Martin <petemartinuk@gmail.com>
2020-03-31 23:04:36 +0000

    visual bug fixes

--- app/components/cases-chart.js
+++ app/components/cases-chart.js
@@ -58,7 +58,7 @@ export default class CasesChartComponent extends Component {
 
   @computed('this.sortedData.@each.total')
   get totalsDataSet() {
-    return this.getDataSet(this.options.dataSets.totals, this.intl.t('total_cases'), true, this.constants.colors.totals);
+    return this.getDataSet(this.options.dataSets.totals, this.intl.t('total_cases'), false, this.constants.colors.totals);
   }
 
   @computed('this.sortedData.@each.saoMiguel')
